ed1e9385cff1ea65a8e1dc10178fdf53009991c9
NikolaGersevica <125268966+NikolaGersevica@users.noreply.github.com>
2023-03-18 21:18:34 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,9 +2,27 @@
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
+for i in range(n // 2, -1, -1):
+        sift_down(i, data, swaps)
+    return swaps
+
+def sift_down(i, data, swaps):
+    n = len(data)
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(min_index, data, swaps)
 
 
     return swaps
